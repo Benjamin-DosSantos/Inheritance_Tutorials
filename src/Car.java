ddca2a6dacb796248eae6635fc54bd06f7eb4127
Benjamin DosSantos <BlackPenSoftware@Gmail.com>
2016-03-17 16:48:39 -0400

    First push of old projects to GitHub

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+/*
+ * 
+ * @Author Benjamin DosSantos
+ * 
+ */
+public class Car {
+	static String name = "Car";
+	
+	public static String getName(){
+		return name;
+	}// End of getName
+}// End of class
---
+++ src/Kia.java
@@ -0,0 +1,6 @@
+
+public class Kia extends Car{
+	public static void main(String args[]){
+		
+	}//End of main method
+}// End of class
